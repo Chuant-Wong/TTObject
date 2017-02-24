d906920f1b996121d930734a8f579a8eee42f37d
王传涛 <15895425989>
2017-02-24 13:46:23 +0800

    Initial Commit

---
+++ TTObject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTObject
+//
+//  Created by Wong on 17/2/24.
+//  Copyright © 2017年 TTObject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TTObject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TTObject
+//
+//  Created by Wong on 17/2/24.
+//  Copyright © 2017年 TTObject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
